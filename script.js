9a7928dea69a7dee1919128c2afa247f548172d9
Sohail702 <118183667+Sohail702@users.noreply.github.com>
2024-01-21 21:07:03 +0530

    script.js

--- script.js
+++ script.js
@@ -1,111 +1,48 @@
-window.addEventListener("DOMContentLoaded", function () {
-    var banner = document.querySelector(".banner");
-    var bannerImage = document.querySelector(".banner .left-img");
-    var bannerHeading = document.querySelector(".banner .heading h1");
-    var bannerText = document.querySelector(".banner .heading p");
+document.addEventListener("DOMContentLoaded", function () {
+    const playButtons = document.querySelectorAll(".fa-play");
+    let currentAudio = null;
   
-    // Set the default banner image and text
-    banner.style.backgroundImage = "url(images/banner1.svg)";
-    bannerImage.setAttribute("src", "images/image.png");
-    bannerHeading.textContent = "Deep Focus";
-    bannerText.textContent = "Keep your focus and enjoy the serenity";
+    playButtons.forEach((button, index) => {
+      button.addEventListener("click", function () {
+        const songFiles = [
+          "music/chaleya.mp3",
+          "music/khairiyat.mp3",
+          "music/mere_yaara.mp3",
+          "music/heeriye.mp3",
+          "music/shayad.mp3",
+          "music/ae_watan.mp3",
+          "music/closer.mp3",
+          "music/drivers_license.mp3",
+          "music/shape_of_you.mp3",
+          "music/see_you_again.mp3",
+          "music/skyfall.mp3",
+          "music/perfect.mp3",
+        ];
+  
+        const audio = new Audio(songFiles[index]);
+  
+        if (currentAudio && currentAudio !== audio) {
+          currentAudio.pause();
+          currentAudio.currentTime = 0; 
+          const prevButton = currentAudio.parentNode.querySelector(".fa-pause");
+          if (prevButton) {
+            prevButton.classList.remove("fa-pause");
+            prevButton.classList.add("fa-play");
+          }
+        }
+  
+        if (audio.paused) {
+          audio.play();
+          button.classList.remove("fa-play");
+          button.classList.add("fa-pause");
+        } else {
+          audio.pause();
+          button.classList.remove("fa-pause");
+          button.classList.add("fa-play");
+        }
+  
+        currentAudio = audio;
+      });
+    });
   });
   
-function playAll(songNumber){
-    var audioElement= document.getElementById("song");
-    var playBtn=document.getElementById("play-pause");
-    var songFileName = "music/"+songNumber+".mp3";
-    var currentRow= document.getElementById("song"+songNumber);
-    var banner = document.querySelector(".banner");
-    var bannerImage = document.querySelector(".banner .left-img");
-    var bannerHeading = document.querySelector(".banner .heading h1");
-    var bannerText = document.querySelector(".banner .heading p");
-
-    if(audioElement.getAttribute("src")==songFileName && !audioElement.paused){
-        audioElement.pause();
-        playBtn.innerHTML='<img src="images/play-button.svg">';
-        
-        banner.style.backgroundImage = "url(images/banner1.svg)";
-        bannerImage.setAttribute("src", "images/image.png");
-        bannerHeading.textContent = "Deep Focus";
-        bannerText.textContent = "Keep your focus and enjoy the serenity";
-    }
-    else{
-        audioElement.setAttribute("src",songFileName);
-        audioElement.play();
-        playBtn.innerHTML='<img src="images/pause-button.svg">';
-       
-        if (songNumber === 1) {
-            banner.style.backgroundImage = "url(images/b01.png)";
-            bannerImage.setAttribute("src", "images/image 1.png");
-            bannerHeading.textContent = "Mike Posner - I took a pill in Ibiza (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
-        } else if (songNumber === 2) {
-            banner.style.backgroundImage = "url(images/b02.png)";
-            bannerImage.setAttribute("src", "images/image 2.png");
-            bannerHeading.textContent = "Ghar (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
-        }
-        else if (songNumber === 3) {
-            banner.style.backgroundImage = "url(images/b03.png)";
-            bannerImage.setAttribute("src", "images/image 3.png");
-            bannerHeading.textContent = "Kahi to hogi woh (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
-        }
-        else if (songNumber === 4) {
-            banner.style.backgroundImage = "url(images/b04.png)";
-            bannerImage.setAttribute("src", "images/image 4.jpg");
-            bannerHeading.textContent = "Kasoor (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
-        }
-        else if (songNumber === 5) {
-            banner.style.backgroundImage = "url(images/b05.png)";
-            bannerImage.setAttribute("src", "images/image 5.png");
-            bannerHeading.textContent = "Tu Hai Kahan";
-            bannerText.textContent = "By Ishaan Kaushik";
-        }
-        else if (songNumber === 6) {
-            banner.style.backgroundImage = "url(images/b06.png)";
-            bannerImage.setAttribute("src", "images/image 6.png");
-            bannerHeading.textContent = "Baarishein - Anuv Jain (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
-        }
-        else if (songNumber === 7) {
-            banner.style.backgroundImage = "url(images/b07.png)";
-            bannerImage.setAttribute("src", "images/image 7.png");
-            bannerHeading.textContent = "Waqt Ki Baatein (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
-        }
-        else if (songNumber === 8) {
-            banner.style.backgroundImage = "url(images/b08.png)";
-            bannerImage.setAttribute("src", "images/image 8.png");
-            bannerHeading.textContent = "Lovely feat. Anika Vidyarthi (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
-        }
-        else if (songNumber === 9) {
-            banner.style.backgroundImage = "url(images/b09.png)";
-            bannerImage.setAttribute("src", "images/image 9.png");
-            bannerHeading.textContent = "Let me down slowly - Alec Benjamin (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
-        }
-        else if (songNumber === 10) {
-            banner.style.backgroundImage = "url(images/b010.png)";
-            bannerImage.setAttribute("src", "images/image 10.png");
-            bannerHeading.textContent = "Jee Le Zara - (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
-        }
-    }
-}
-
-function playaudio(){
-    var audio=document.getElementById("song");
-    var playBtn=document.getElementById("play-pause");
-    if(audio.paused){
-        audio.play();
-        playBtn.innerHTML='<img src="images/pause-button.svg">';
-    }
-    else{
-        audio.pause();
-        playBtn.innerHTML='<img src="images/play-button.svg">';
-    }
-}
